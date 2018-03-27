4a76ff40c208b49123be212f687253256ee7360e
debbie <drosenfeld87@gmail.com>
2018-03-27 14:19:30 -0400

    complete assignment'

--- election.js
+++ election.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+    var ul = document.getElementById('list');
 
+    var request = $.ajax ({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    data: {},
+    dataType: 'json'
+  }).done(function(data){
+    data.candidates.forEach(function(candidate){
+      var li = document.createElement('li');
+      ul.appendChild(li);
+      li.innerHTML = candidate.name + ": " + "Votes " + candidate.votes;
+    })
+  })
 });
